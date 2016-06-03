f5de379f2db9e0807dbfa93f1f59c735989ce093
Xing <sky00000kimo@gmail.com>
2016-06-03 10:25:33 +0800

    add edit in index

--- app/controllers/salesmen_controller.rb
+++ app/controllers/salesmen_controller.rb
@@ -1,21 +1,51 @@
 class SalesmenController < ApplicationController
-  before_action:set_salesman_find,:only =>[:show, :edit, :delete]
+  before_action:set_salesman_find,:only =>[:show, :edit,:update, :destroy]
 
   def index
-	@salesmen = Salesman.page(params[:page]).per(10)
+	@salesmen = Salesman.page(params[:page]).per(5)
+    @salesman = Salesman.new
   end
   
   def show
+
   end
 
+  def new
+    @salesmen = Salesman.page(params[:page]).per(5)
+    @salesman = Salesman.new
+    redirect_to salesmen_url	 
+  end
+
+  def create
+    @salesman = Salesman.new(salesman_params)
+    @salesman.save
+
+    redirect_to salesmen_path
+  end
 
   def edit
+    @salesmen = Salesman.page(params[:page]).per(5)
+    render :action => :index
   end
 
-  def delete
+  def update
+   	@salesman.update(salesman_params)
+    @salesmen = Salesman.page(params[:page]).per(5)
+  	# redirect_to salesmen_url(@salesmen)
+  	render :action => :index
+  end
+
+
+  def destroy
+  	@salesman.destroy
+  	redirect_to salesmen_path
   end
 
   private
+
+  def salesman_params
+  	params.require(:salesman).permit(:name, :email, :agent_id)
+  end
 
   def set_salesman_find
 	  	@salesman = Salesman.find(params[:id])
